0f850072cc31328ba638fe165049d6aea05b1f22
HadertS <source.zero@googlemail.com>
2023-02-06 13:59:43 +0000

    updated dB tables

--- rpg-books.sql
+++ rpg-books.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.5-10.10.2-MariaDB-1:10.10.2+maria~ubu2204)
 # Database: rpg-books
-# Generation Time: 2023-02-06 12:15:35 +0000
+# Generation Time: 2023-02-06 13:57:25 +0000
 # ************************************************************
 
 
@@ -27,11 +27,11 @@ DROP TABLE IF EXISTS `books`;
 
 CREATE TABLE `books` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL DEFAULT '',
-  `price_paid` float NOT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `price_paid` float(5,2) NOT NULL,
   `aquisition_date` date NOT NULL,
   `image_source` varchar(2048) NOT NULL DEFAULT '',
-  `deleted` tinyint(1) NOT NULL,
+  `deleted` tinyint(1) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,9 +41,9 @@ LOCK TABLES `books` WRITE;
 INSERT INTO `books` (`id`, `title`, `price_paid`, `aquisition_date`, `image_source`, `deleted`)
 VALUES
 	(1,'The Riddle of Steel',53.95,'2017-06-21','https://upload.wikimedia.org/wikipedia/en/2/2a/Riddle_of_steel_rpg_cover.gif',0),
-	(2,'AD&D 1st Edition Dungeon Masters Guide',0.5,'2000-01-01','https://upload.wikimedia.org/wikipedia/en/8/8c/DungeonMasterGuide4Cover.jpg',0),
-	(3,'AD&D 1st Edition Monster Manual (UK softcover)',10,'2014-05-01','https://upload.wikimedia.org/wikipedia/en/a/ac/MonsterManual-1stEdAD%26D-Cover.jpg',0),
-	(4,'Forbidden Lands Boxed Set',40,'2019-11-29','https://upload.wikimedia.org/wikipedia/en/3/3e/Cover_of_Forbidden_Lands_RPG.png',0);
+	(2,'AD&D 1st Edition Dungeon Masters Guide',0.50,'2000-01-01','https://upload.wikimedia.org/wikipedia/en/8/8c/DungeonMasterGuide4Cover.jpg',0),
+	(3,'AD&D 1st Edition Monster Manual (UK softcover)',10.00,'2014-05-01','https://upload.wikimedia.org/wikipedia/en/a/ac/MonsterManual-1stEdAD%26D-Cover.jpg',0),
+	(4,'Forbidden Lands Boxed Set',40.00,'2019-11-29','https://upload.wikimedia.org/wikipedia/en/3/3e/Cover_of_Forbidden_Lands_RPG.png',0);
 
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
