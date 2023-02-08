ebdf36eb670d3840d7fc5973f6d85fe7a652782c
HadertS <source.zero@googlemail.com>
2023-02-08 16:53:38 +0000

    EOD WIP of sanitize/validate input form

--- databaseFunctions.php
+++ databaseFunctions.php
@@ -22,6 +22,28 @@ function createDBConnection(string $dbName,string $username,string $password):PD
 function retrieveCollectionData(PDO $db):array{
     $stmnt = $db->prepare('SELECT `title`,`price_paid`,`acquisition_date`,`image_source`FROM `books` WHERE `deleted`=0;');
     $stmnt->execute();
-
     return $stmnt->fetchAll();
+}
+
+/**
+ * @param array $dirtyArray
+ * @return array
+ */
+function sanitiseInput(array $dirtyArray):array{
+    $sanitisedArray['title'] = filter_var($dirtyArray['title'], FILTER_SANITIZE_STRING);
+    $sanitisedArray['price_paid'] = filter_var($dirtyArray['price_paid'], FILTER_SANITIZE_NUMBER_FLOAT);
+    $sanitisedArray['acquisition_date'] = filter_var($dirtyArray['acquisition_date'], FILTER_SANITIZE_STRING);
+    $sanitisedArray['image_source'] = filter_var($dirtyArray['image_source'], FILTER_SANITIZE_URL);
+    return $sanitisedArray;
+}
+
+/**
+ * @param array $sanitisedArray
+ * @return array
+ */
+function validateInput(array $sanitisedArray):array{
+    $validatedArray['title'] = filter_var($sanitisedArray['title'], FILTER_SANITIZE_STRING);
+    $validatedArray['price_paid'] = filter_var($sanitisedArray['price_paid'], FILTER_SANITIZE_NUMBER_FLOAT);
+    $validatedArray['acquisition_date'] = filter_var($sanitisedArray['acquisition_date'], FILTER_SANITIZE_STRING);
+    $validatedArray['image_source'] = filter_var($sanitisedArray['image_source'], FILTER_SANITIZE_URL);
 }
--- insert-book.php
+++ insert-book.php
@@ -1,1 +1,8 @@
 <?php
+require_once "databaseFunctions.php";
+
+$formInput = $_POST;
+if (!isset($formInput['title'],$formInput['price_paid'],$formInput['acquisition_date'],$formInput['image_source'])){
+    header('Location: add-new-book.php');
+    die();
+}
